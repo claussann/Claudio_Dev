d7587ff1897b75479c5ff684b3db431490807967
claussann <clausann92@gmail.com>
2025-06-29 23:27:21 +0200

    feat: implement email sendbox with text area in modal

--- src/Components/Contacts.tsx
+++ src/Components/Contacts.tsx
@@ -1,19 +1,25 @@
+import EmailModal from "./EmailModal"
+import { useState } from "react"
+
 function Contacts() {
+    const [showModal, setShowModal] = useState(false);
+
     return (
         <>
             <div className='col-3 col-md-1'>
-                    <img className='icon img-fluid' src="email.png" alt="email" />
+                    <img className='icon img-fluid' src="email.png" alt="Contact me" title="Contact me" onClick={() => setShowModal(true)} />
             </div>
             <div className='col-3 col-md-1'>
                 <a href="https://www.linkedin.com/in/claudio-sanna-127326349/" target="_blank">
-                    <img className='icon img-fluid' src="linkedin.png" alt="linkedin" />
+                    <img className='icon img-fluid' src="linkedin.png" alt="Linkedin" title="LinkedIn" />
                 </a>
             </div>
             <div className='col-3 col-md-1'>
                 <a href="http://github.com/claussann" target="_blank" rel="noopener noreferrer">
-                    <img className='icon img-fluid' src="github.png" alt="github" />
+                    <img className='icon img-fluid' src="github.png" alt="Github" title="GitHub" />
                 </a>
             </div>
+            {<EmailModal openModal={showModal} closeModal={() => setShowModal(false)} />}
         </>
     )
 }
--- src/Components/EmailModal.tsx
+++ src/Components/EmailModal.tsx
@@ -1,5 +1,76 @@
-function EmailModal(){
-    return(<>
-        
-    </>)
+import { Button, Modal } from "react-bootstrap"
+import { useRef } from "react"
+import emailjs from '@emailjs/browser';
+
+type Props = {
+    openModal: boolean
+    closeModal: () => void
 }
+function EmailModal({ openModal, closeModal }: Props) {
+
+    const form = useRef<HTMLFormElement>(null);
+    const sendMail = (e: any) => {
+        e.preventDefault();
+
+        if (!form.current) return;
+        emailjs
+            .sendForm('service_f7y6pw8', 'template_fhxf5jr', form.current, {
+                publicKey: 'mVotJ0v4MTpuKIBoO',
+            })
+            .then(
+                () => {
+                    alert('email successfully sent!');
+                },
+                (error) => {
+                    alert('Failed to send email...' + error.text);
+                },
+            );
+        closeModal();
+    };
+
+    return (
+        <Modal show={openModal} onHide={closeModal}>
+            <div style={{ borderRadius: "20px", border: "2px solid grey" }}>
+                <Modal.Header style={{
+                    background: "linear-gradient(to left, rgb(12, 12, 12), rgb(54, 53, 53))",
+                    display: "flex",
+                    justifyContent: "center"
+                }}>
+                    <Modal.Title>
+                        <h4 style={{ fontFamily: "Arial" }}>Send me an email</h4>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{ width: "100%", background: "linear-gradient(to left, rgb(12, 12, 12), rgb(54, 53, 53))" }}>
+                    <div className="m-2 form-group col-12">
+                        <form ref={form} onSubmit={sendMail}>
+                            <textarea style={{
+                                width: "100%",
+                                height: "200px",
+                                background: "linear-gradient(to left, rgb(12, 12, 12), rgb(54, 53, 53))",
+                                color: "white"
+                            }}
+                                className="form-control"
+                                placeholder="Your message here"
+                                name="message">
+                            </textarea>
+                            <div className="col-12 d-flex justify-content-center">
+                                <Button className="mt-2 col-12 col-md-3" variant="outline-light" type="submit">
+                                    Send email
+                                </Button>
+                            </div>
+                        </form>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer style={{ background: "linear-gradient(to left, rgb(12, 12, 12), rgb(54, 53, 53))" }}>
+                    <Button variant="dark" onClick={closeModal}>
+                        Close
+                    </Button>
+
+                </Modal.Footer>
+            </div>
+        </Modal>
+    )
+}
+
+
+export default EmailModal
