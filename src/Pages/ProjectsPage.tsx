b40447db68b7ae518496d2ba24969ce7c3bbbdd8
claussann <clausann92@gmail.com>
2025-07-04 19:58:59 +0200

    project image fixed

--- src/Pages/ProjectsPage.tsx
+++ src/Pages/ProjectsPage.tsx
@@ -25,19 +25,28 @@ function ProjectsPage() {
             <div className="projects row mt-3 border border-light d-flex justify-content-center" style={{ borderRadius: "10px" }} >
                 <div className='col-10 text-center p-3'>
                     <p>Music_Project_Demo</p>
-                    <a href="https://music-portfolio-project.netlify.app/" target="_blank" rel="noopener noreferrer"><div id="music" style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto"></div></a>
+                    <a href="https://music-portfolio-project.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        <div id="music" style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto">
+                            <img className='img-fluid' style={{ width: "100%", height: "100%", borderRadius: "10px", objectFit: "cover" }} src="img_rock.png" alt="music image" />
+                        </div>
+                    </a>
                 </div>
             </div>
             <div className="projects row mt-3 border border-light d-flex justify-content-center" style={{ borderRadius: "10px" }} >
                 <div className='col-10 text-center p-3'>
                     <p>eCommerce_Project_Demo</p>
-                    <a href="https://e-portfolio-commerce.netlify.app/" target="_blank" rel="noopener noreferrer"><div id="eCommerce" style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto"></div></a>
+                    <a href="https://e-portfolio-commerce.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        <div id="eCommerce" style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto">
+                            <img className='img-fluid' style={{ width: "100%", height: "100%", borderRadius: "10px", objectFit: "cover" }} src="eCommerce.png" alt="eCommerce image" />
+                        </div></a>
                 </div>
             </div>
             <div className="projects row mt-3 border border-light d-flex justify-content-center" style={{ borderRadius: "10px" }} >
                 <div className='col-10 text-center p-3'>
                     <p>Fishing_inventory_Video</p>
-                    <div id="fishing_inventory" onClick={openFishingModal} style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto"></div>
+                    <div id="fishing_inventory" onClick={openFishingModal} style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto">
+                        <img className='img-fluid' style={{ width: "100%", height: "100%", borderRadius: "10px", objectFit: "cover" }} src="Pesca.png" alt="fishing image" />
+                    </div>
                 </div>
             </div>
             {fishingModal && <FishingVideo openModal={fishingModal} onClose={closeFishingModal} />}
