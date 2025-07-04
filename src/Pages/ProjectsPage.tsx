e58e189b16318fcf64b14198c09848c328f3f8fd
claussann <clausann92@gmail.com>
2025-07-04 19:52:58 +0200

    fix button become div

--- src/Pages/ProjectsPage.tsx
+++ src/Pages/ProjectsPage.tsx
@@ -25,19 +25,19 @@ function ProjectsPage() {
             <div className="projects row mt-3 border border-light d-flex justify-content-center" style={{ borderRadius: "10px" }} >
                 <div className='col-10 text-center p-3'>
                     <p>Music_Project_Demo</p>
-                    <a href="https://music-portfolio-project.netlify.app/" target="_blank" rel="noopener noreferrer"><button id="music" style={{ height: "200px" }} className="btn btn-outline-light col-12 mt-2 mb-2 mx-auto"></button></a>
+                    <a href="https://music-portfolio-project.netlify.app/" target="_blank" rel="noopener noreferrer"><div id="music" style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto"></div></a>
                 </div>
             </div>
             <div className="projects row mt-3 border border-light d-flex justify-content-center" style={{ borderRadius: "10px" }} >
                 <div className='col-10 text-center p-3'>
                     <p>eCommerce_Project_Demo</p>
-                    <a href="https://e-portfolio-commerce.netlify.app/" target="_blank" rel="noopener noreferrer"><button id="eCommerce" style={{ height: "200px" }} className="btn btn-outline-light col-12 mt-2 mb-2 mx-auto"></button></a>
+                    <a href="https://e-portfolio-commerce.netlify.app/" target="_blank" rel="noopener noreferrer"><div id="eCommerce" style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto"></div></a>
                 </div>
             </div>
             <div className="projects row mt-3 border border-light d-flex justify-content-center" style={{ borderRadius: "10px" }} >
                 <div className='col-10 text-center p-3'>
                     <p>Fishing_inventory_Video</p>
-                    <button id="fishing_inventory" onClick={openFishingModal} style={{ height: "200px" }} className="btn btn-outline-light col-12 mt-2 mb-2 mx-auto"></button>
+                    <div id="fishing_inventory" onClick={openFishingModal} style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto"></div>
                 </div>
             </div>
             {fishingModal && <FishingVideo openModal={fishingModal} onClose={closeFishingModal} />}
