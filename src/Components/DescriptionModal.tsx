496087922d2378116532026d8361d67feb4d1a75
claussann <clausann92@gmail.com>
2025-07-06 19:23:17 +0200

    implement description modal and JSON file for description string

---
+++ src/Components/DescriptionModal.tsx
@@ -0,0 +1,39 @@
+import { Button, Modal } from "react-bootstrap"
+
+type DescriptionModalProps = {
+    openModal: boolean;
+    onClose: () => void;
+    description: string;
+    title: string
+};
+
+function DescriptionModal({ title, openModal, onClose, description }: DescriptionModalProps) {
+    return (
+        <Modal show={openModal} onHide={onClose}>
+            <div style={{ borderRadius: "20px", border: "2px solid grey" }}>
+                <Modal.Header style={{
+                    background: "linear-gradient(to left, rgb(12, 12, 12), rgb(54, 53, 53))",
+                    display: "flex",
+                    justifyContent: "center"
+                }}>
+                    <Modal.Title>
+                        <h4 style={{ fontFamily: "Arial" }}>{title}</h4>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{ width: "100%", background: "linear-gradient(to left, rgb(12, 12, 12), rgb(54, 53, 53))" }}>
+                    <div className="form-group col-12">
+                        <p>{description}</p>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer style={{ background: "linear-gradient(to left, rgb(12, 12, 12), rgb(54, 53, 53))" }}>
+                    <Button variant="dark" onClick={onClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </div>
+        </Modal>
+    )
+
+}
+
+export default DescriptionModal
--- src/Pages/ProjectsPage.tsx
+++ src/Pages/ProjectsPage.tsx
@@ -1,16 +1,34 @@
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+
 import FishingVideo from '../Components/FishingVideo'
+import DescriptionModal from '../Components/DescriptionModal';
+import proJectDataRow from '../Data/Projects.json'
 
 import '../App.css'
 
+type project = {
+    id: number,
+    title: string,
+    description: string
+}
+
 function ProjectsPage() {
+    const [showDescriptionMusicModal, setShowDescriptionMusicModal] = useState(false);
+    const [showDescriptionEcommerceModal, setShowDescriptionEcommerceModal] = useState(false);
+    const [showDescriptionFishingModal, setShowDescriptionFishingModal] = useState(false);
     const [fishingModal, setFishingModal] = useState(false);
+
     const navigate = useNavigate()
-    const goToHOme = () => navigate("/home")
 
     const openFishingModal = () => setFishingModal(true)
     const closeFishingModal = () => setFishingModal(false)
+
+    const goToHOme = () => navigate("/home")
+
+    const projectData: project[] = proJectDataRow.projects
+
+
     return (
         <div id="projectsPage" className="container">
             <div className="projects row border border-light mt-3 text-center d-flex" style={{ borderRadius: "10px" }}>
@@ -24,32 +42,46 @@ function ProjectsPage() {
             </div>
             <div className="projects row mt-3 border border-light d-flex justify-content-center" style={{ borderRadius: "10px" }} >
                 <div className='col-10 text-center p-3'>
-                    <p>Music_Project_Demo</p>
+                    <p>{projectData[0].title}</p>
                     <a href="https://music-portfolio-project.netlify.app/" target="_blank" rel="noopener noreferrer">
                         <div id="music" style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto">
                             <img className='img-fluid' style={{ width: "100%", height: "100%", borderRadius: "10px", objectFit: "cover" }} src="img_rock.png" alt="music image" />
                         </div>
                     </a>
+                    <div className="col-12 mt-2 p-2 d-flex justify-content-center">
+                        <button className='btn btn-outline-light' onClick={() => setShowDescriptionMusicModal(true)}>Description</button>
+                    </div>
                 </div>
             </div>
             <div className="projects row mt-3 border border-light d-flex justify-content-center" style={{ borderRadius: "10px" }} >
                 <div className='col-10 text-center p-3'>
-                    <p>eCommerce_Project_Demo</p>
+                    <p>{projectData[1].title}</p>
                     <a href="https://e-portfolio-commerce.netlify.app/" target="_blank" rel="noopener noreferrer">
                         <div id="eCommerce" style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto">
                             <img className='img-fluid' style={{ width: "100%", height: "100%", borderRadius: "10px", objectFit: "cover" }} src="eCommerce.png" alt="eCommerce image" />
-                        </div></a>
+                        </div>
+                    </a>
+                    <div className="col-12 mt-2 p-2 d-flex justify-content-center">
+                        <button className='btn btn-outline-light' onClick={() => setShowDescriptionEcommerceModal(true)}>Description</button>
+                    </div>
                 </div>
             </div>
             <div className="projects row mt-3 border border-light d-flex justify-content-center" style={{ borderRadius: "10px" }} >
                 <div className='col-10 text-center p-3'>
-                    <p>Fishing_inventory_Video</p>
+                    <p>{projectData[2].title}</p>
                     <div id="fishing_inventory" onClick={openFishingModal} style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto">
                         <img className='img-fluid' style={{ width: "100%", height: "100%", borderRadius: "10px", objectFit: "cover" }} src="Pesca.png" alt="fishing image" />
+                    </div>
+                    <div className="col-12 mt-2 p-2 d-flex justify-content-center">
+                        <button className='btn btn-outline-light' onClick={() => setShowDescriptionFishingModal(true)}>Description</button>
                     </div>
                 </div>
             </div>
             {fishingModal && <FishingVideo openModal={fishingModal} onClose={closeFishingModal} />}
+            {showDescriptionMusicModal && <DescriptionModal openModal={showDescriptionMusicModal} onClose={() => setShowDescriptionMusicModal(false)} title={projectData[0].title} description={projectData[0].description} />}
+            {showDescriptionEcommerceModal && <DescriptionModal openModal={showDescriptionEcommerceModal} onClose={() => setShowDescriptionEcommerceModal(false)} title={projectData[1].title} description={projectData[1].description} />}
+            {showDescriptionFishingModal && <DescriptionModal openModal={showDescriptionFishingModal} onClose={() => setShowDescriptionFishingModal(false)} title={projectData[2].title} description={projectData[2].description} />}
+
         </div>
     )
 }
