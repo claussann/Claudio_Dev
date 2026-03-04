4ccab862a867b429358c3ab468461d7e2c849362
claussann <clausann92@gmail.com>
2026-03-04 22:48:57 +0100

    change fishing inventory project from old to new version online on fishing.inventory.it

--- src/Pages/ProjectsPage.tsx
+++ src/Pages/ProjectsPage.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 
-import FishingVideo from '../Components/FishingVideo'
 import DescriptionModal from '../Components/DescriptionModal';
 import proJectDataRow from '../Data/Projects.json'
 
@@ -17,12 +16,8 @@ function ProjectsPage() {
     const [showDescriptionMusicModal, setShowDescriptionMusicModal] = useState(false);
     const [showDescriptionEcommerceModal, setShowDescriptionEcommerceModal] = useState(false);
     const [showDescriptionFishingModal, setShowDescriptionFishingModal] = useState(false);
-    const [fishingModal, setFishingModal] = useState(false);
 
     const navigate = useNavigate()
-
-    const openFishingModal = () => setFishingModal(true)
-    const closeFishingModal = () => setFishingModal(false)
 
     const goToHOme = () => navigate("/home")
 
@@ -69,15 +64,16 @@ function ProjectsPage() {
             <div className="projects row mt-3 border border-light d-flex justify-content-center" style={{ borderRadius: "10px" }} >
                 <div className='col-10 text-center p-3'>
                     <p>{projectData[2].title}</p>
-                    <div id="fishing_inventory" onClick={openFishingModal} style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto">
+                    <a href="https://fishing-inventory.it/" target="_blank" rel="noopener noreferrer">
+                    <div id="fishing_inventory" style={{ height: "200px" }} className="border border-light col-12 mt-2 mb-2 mx-auto">
                         <img className='img-fluid' style={{ width: "100%", height: "100%", borderRadius: "10px", objectFit: "cover" }} src="Pesca.png" alt="fishing image" />
                     </div>
+                    </a>
                     <div className="col-12 mt-2 p-2 d-flex justify-content-center">
                         <button className='btn btn-outline-light' onClick={() => setShowDescriptionFishingModal(true)}>Description</button>
                     </div>
                 </div>
             </div>
-            {fishingModal && <FishingVideo openModal={fishingModal} onClose={closeFishingModal} />}
             {showDescriptionMusicModal && <DescriptionModal openModal={showDescriptionMusicModal} onClose={() => setShowDescriptionMusicModal(false)} title={projectData[0].title} description={projectData[0].description} />}
             {showDescriptionEcommerceModal && <DescriptionModal openModal={showDescriptionEcommerceModal} onClose={() => setShowDescriptionEcommerceModal(false)} title={projectData[1].title} description={projectData[1].description} />}
             {showDescriptionFishingModal && <DescriptionModal openModal={showDescriptionFishingModal} onClose={() => setShowDescriptionFishingModal(false)} title={projectData[2].title} description={projectData[2].description} />}
